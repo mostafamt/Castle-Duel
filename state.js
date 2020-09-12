7ee86b947980ab9b87a13f53590da25c29023376
Mostafa <mostafa@localhost.localdomain>
2020-09-12 02:24:03 +0200

    first commit

---
+++ state.js
@@ -0,0 +1,54 @@
+// Some usefull variables
+var maxHealth = 10
+var maxFood = 10
+var handSize = 5
+var cardUid = 0
+var currentPlayingCard = null
+
+// The consolidated state of our app
+var state = {
+  // World
+  worldRatio: getWorldRatio(),
+  // TODO Other things
+  turn: 1,
+  drawPile: pile,
+  discardPile: {},
+  canPlay: false,
+  players: [
+    {
+      name: 'Anne of Cleves',
+      food: 10,
+      health: 10,
+      skipTurn: false,
+      skippedTurn: false,
+      hand: [],
+      lastPlayedCardId: null,
+      dead: false,
+    },
+    {
+      name: 'William the Bald',
+      food: 10,
+      health: 10,
+      skipTurn: false,
+      skippedTurn: false,
+      hand: [],
+      lastPlayedCardId: null,
+      dead: false,
+    }
+  ],
+  currentPlayerIndex: Math.round(Math.random()),
+  testHand: [],
+  activeOverlay: null,
+  get currentPlayer(){
+    return state.players[state.currentPlayerIndex];
+  },
+  get currentOpponentId() {
+    return state.currentPlayerIndex === 0 ? 1 : 0 ;
+  },
+  get currentOpponent() {
+    return state.players[state.currentOpponentId];
+  },
+  get currentHand(){
+    return state.currentPlayer.hand;
+  },
+}
